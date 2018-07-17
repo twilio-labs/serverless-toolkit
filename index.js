25cadd18b80d04ba13447649d1d36d7096e6f18d
Dominik Kundel <dominik.kundel@gmail.com>
2018-07-17 17:15:47 +0200

    :bug: Fix typo in function name

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const { runServer } = require('./src/server');
 
 module.exports = {
   runDevServer: runServer,
-  handleToExpressRoute: functionToRoute
+  handlerToExpressRoute: functionToRoute
 };
