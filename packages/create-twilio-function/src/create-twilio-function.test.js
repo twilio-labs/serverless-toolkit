ecba8a4acca4e4292b57ab9681bd23fec5fa8ba9
Phil Nash <philnash@gmail.com>
2019-01-30 10:46:44 +1100

    Break out parts of createTwilioFunction for easier testing and isolation.

--- packages/create-twilio-function/src/create-twilio-function.test.js
+++ packages/create-twilio-function/src/create-twilio-function.test.js
@@ -1,53 +1,5 @@
 const createTwilioFunction = require('./create-twilio-function');
-const inquirer = require('inquirer');
 
-jest.mock('inquirer');
-
-describe('create-twilio-function', () => {
-  beforeEach(() => jest.clearAllMocks());
-
-  test(`should ask for an accountSid if not specified`, async () => {
-    inquirer.prompt = jest.fn(() =>
-      Promise.resolve({
-        accountSid: 'test-sid',
-        authToken: 'test-auth-token'
-      })
-    );
-    await createTwilioFunction({
-      name: 'function-test'
-    });
-    expect(inquirer.prompt).toHaveBeenCalledTimes(1);
-    expect(inquirer.prompt).toHaveBeenCalledWith(expect.any(Array));
-  });
-
-  test(`should ask for an auth if not specified`, async () => {
-    inquirer.prompt = jest.fn(() =>
-      Promise.resolve({
-        accountSid: 'test-sid',
-        authToken: 'test-auth-token'
-      })
-    );
-    await createTwilioFunction({
-      name: 'function-test',
-      accountSid: 'test-sid'
-    });
-    expect(inquirer.prompt).toHaveBeenCalledTimes(1);
-    expect(inquirer.prompt).toHaveBeenCalledWith(expect.any(Array));
-  });
-
-  test(`should not prompt if account sid and auth token specified`, async () => {
-    inquirer.prompt = jest.fn(() =>
-      Promise.resolve({
-        accountSid: 'test-sid',
-        authToken: 'test-auth-token'
-      })
-    );
-    await createTwilioFunction({
-      name: 'function-test',
-      accountSid: 'test-sid',
-      authToken: 'test-auth-token'
-    });
-    expect(inquirer.prompt).toHaveBeenCalledTimes(1);
-    expect(inquirer.prompt).toHaveBeenCalledWith([]);
-  });
+describe('createTwilioFunction', () => {
+  test.skip('it creates a Twilio Function', () => {});
 });
---
+++ packages/create-twilio-function/src/create-twilio-function/prompt.test.js
@@ -0,0 +1,53 @@
+const { promptForAccountDetails } = require('./prompt');
+const inquirer = require('inquirer');
+
+jest.mock('inquirer');
+
+describe('promptForAccountDetails', () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  test(`should ask for an accountSid if not specified`, async () => {
+    inquirer.prompt = jest.fn(() =>
+      Promise.resolve({
+        accountSid: 'test-sid',
+        authToken: 'test-auth-token'
+      })
+    );
+    await promptForAccountDetails({
+      name: 'function-test'
+    });
+    expect(inquirer.prompt).toHaveBeenCalledTimes(1);
+    expect(inquirer.prompt).toHaveBeenCalledWith(expect.any(Array));
+  });
+
+  test(`should ask for an auth if not specified`, async () => {
+    inquirer.prompt = jest.fn(() =>
+      Promise.resolve({
+        accountSid: 'test-sid',
+        authToken: 'test-auth-token'
+      })
+    );
+    await promptForAccountDetails({
+      name: 'function-test',
+      accountSid: 'test-sid'
+    });
+    expect(inquirer.prompt).toHaveBeenCalledTimes(1);
+    expect(inquirer.prompt).toHaveBeenCalledWith(expect.any(Array));
+  });
+
+  test(`should not prompt if account sid and auth token specified`, async () => {
+    inquirer.prompt = jest.fn(() =>
+      Promise.resolve({
+        accountSid: 'test-sid',
+        authToken: 'test-auth-token'
+      })
+    );
+    await promptForAccountDetails({
+      name: 'function-test',
+      accountSid: 'test-sid',
+      authToken: 'test-auth-token'
+    });
+    expect(inquirer.prompt).toHaveBeenCalledTimes(1);
+    expect(inquirer.prompt).toHaveBeenCalledWith([]);
+  });
+});
