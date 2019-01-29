61a58790b3f356c1fdcc053377789b510f92758c
Phil Nash <philnash@gmail.com>
2019-01-29 17:54:08 +1100

    Asks for account sid and auth token

---
+++ packages/create-twilio-function/src/create-twilio-function.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+
+async function promptForAccountDetails(config) {
+  const questions = [];
+  if (!config.accountSid) {
+    questions.push({
+      type: 'input',
+      name: 'accountSid',
+      message: 'Twilio Account SID',
+      validate: input =>
+        input.startsWith('AC') ? true : 'An Account SID starts with "AC".'
+    });
+  }
+  if (!config.authToken) {
+    questions.push({
+      type: 'password',
+      name: 'authToken',
+      message: 'Twilio auth token'
+    });
+  }
+  return await inquirer.prompt(questions);
+}
+
+async function createTwilioFunction(config) {
+  const accountDetails = await promptForAccountDetails(config);
+  config = { ...accountDetails, ...config };
+}
+
+module.exports = createTwilioFunction;
