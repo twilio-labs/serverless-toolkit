6aa76824a3f47accac1cafb4a4f32af4e9a2c093
Phil Nash <philnash@gmail.com>
2019-02-01 12:12:22 +1100

    Functions for adding an example function and .env file

--- packages/create-twilio-function/src/create-twilio-function/create-files.test.js
+++ packages/create-twilio-function/src/create-twilio-function/create-files.test.js
@@ -1,4 +1,9 @@
-const { createPackageJSON, createDirectory } = require('./create-files');
+const {
+  createPackageJSON,
+  createDirectory,
+  createExampleFunction,
+  createEnvFile
+} = require('./create-files');
 const mockFs = require('mock-fs');
 const fs = require('fs');
 
@@ -41,6 +46,53 @@ describe('createPackageJSON', () => {
     expect.assertions(1);
     try {
       await createPackageJSON('/test', 'project-name');
+    } catch (e) {
+      expect(e.toString()).toMatch('file already exists');
+    }
+  });
+});
+
+describe('createExampleFunction', () => {
+  test('it creates a new example function', async () => {
+    await createExampleFunction('/test');
+    const file = fs.statSync('/test/example.js');
+    expect(file.isFile());
+    const contents = fs.readFileSync('/test/example.js', { encoding: 'utf-8' });
+    expect(contents).toMatch('Twilio.twiml.VoiceResponse');
+  });
+
+  test('it rejects if there is already a package.json', async () => {
+    fs.closeSync(fs.openSync('/test/example.js', 'w'));
+    expect.assertions(1);
+    try {
+      await createExampleFunction('/test');
+    } catch (e) {
+      expect(e.toString()).toMatch('file already exists');
+    }
+  });
+});
+
+describe('createEnvFile', () => {
+  test('it creates a new .env file', async () => {
+    await createEnvFile('/test', {
+      accountSid: 'AC123',
+      authToken: 'qwerty123456'
+    });
+    const file = fs.statSync('/test/.env');
+    expect(file.isFile());
+    const contents = fs.readFileSync('/test/.env', { encoding: 'utf-8' });
+    expect(contents).toMatch('ACCOUNT_SID=AC123');
+    expect(contents).toMatch('AUTH_TOKEN=qwerty123456');
+  });
+
+  test('it rejects if there is already a package.json', async () => {
+    fs.closeSync(fs.openSync('/test/.env', 'w'));
+    expect.assertions(1);
+    try {
+      await createEnvFile('/test', {
+        accountSid: 'AC123',
+        authToken: 'qwerty123456'
+      });
     } catch (e) {
       expect(e.toString()).toMatch('file already exists');
     }
