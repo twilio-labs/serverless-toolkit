6ec3c97c48d25522d700661f2d0d0f4bbc74ce35
Phil Nash <philnash@gmail.com>
2019-02-01 15:45:04 +1100

    Put things together in create-twilio-function function

--- packages/create-twilio-function/src/create-twilio-function.js
+++ packages/create-twilio-function/src/create-twilio-function.js
@@ -1,8 +1,27 @@
 const { promptForAccountDetails } = require('./create-twilio-function/prompt');
+const {
+  createDirectory,
+  createEnvFile,
+  createExampleFunction,
+  createGitignore,
+  createPackageJSON
+} = require('./create-twilio-function/create-files');
 
 async function createTwilioFunction(config) {
-  const accountDetails = await promptForAccountDetails(config);
+  accountDetails = await promptForAccountDetails(config);
   config = { ...accountDetails, ...config };
+
+  await createDirectory(config.path, config.name);
+  const projectDir = `${config.path}/${config.name}`;
+  await createDirectory(projectDir, 'functions');
+  await createDirectory(projectDir, 'assets');
+  await createEnvFile(projectDir, {
+    accountSid: config.accountSid,
+    authToken: config.authToken
+  });
+  await createGitignore(projectDir);
+  await createExampleFunction(`${projectDir}/functions`);
+  await createPackageJSON(projectDir, config.name);
 }
 
 module.exports = createTwilioFunction;
