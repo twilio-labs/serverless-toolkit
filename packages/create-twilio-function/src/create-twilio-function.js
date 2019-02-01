5f2fbbd922af696bab8ab1c898fe3eab364ef4ef
Phil Nash <philnash@gmail.com>
2019-02-01 17:18:10 +1100

    Doesn't create function if target folder already exists

--- packages/create-twilio-function/src/create-twilio-function.js
+++ packages/create-twilio-function/src/create-twilio-function.js
@@ -6,22 +6,32 @@ const {
   createGitignore,
   createPackageJSON
 } = require('./create-twilio-function/create-files');
+const { promisify } = require('util');
+const access = promisify(require('fs').access);
 
 async function createTwilioFunction(config) {
-  accountDetails = await promptForAccountDetails(config);
-  config = { ...accountDetails, ...config };
-
-  await createDirectory(config.path, config.name);
   const projectDir = `${config.path}/${config.name}`;
-  await createDirectory(projectDir, 'functions');
-  await createDirectory(projectDir, 'assets');
-  await createEnvFile(projectDir, {
-    accountSid: config.accountSid,
-    authToken: config.authToken
-  });
-  await createGitignore(projectDir);
-  await createExampleFunction(`${projectDir}/functions`);
-  await createPackageJSON(projectDir, config.name);
+  return access(projectDir)
+    .then(() => {
+      console.log(
+        `A directory called '${name}' already exists. Please create your function in a new directory.`
+      );
+    })
+    .catch(async () => {
+      accountDetails = await promptForAccountDetails(config);
+      config = { ...accountDetails, ...config };
+
+      await createDirectory(config.path, config.name);
+      await createDirectory(projectDir, 'functions');
+      await createDirectory(projectDir, 'assets');
+      await createEnvFile(projectDir, {
+        accountSid: config.accountSid,
+        authToken: config.authToken
+      });
+      await createGitignore(projectDir);
+      await createExampleFunction(`${projectDir}/functions`);
+      await createPackageJSON(projectDir, config.name);
+    });
 }
 
 module.exports = createTwilioFunction;
