1716fa11a5fe6fccd37dd3a7455aebf5161187ef
Phil Nash <philnash@gmail.com>
2019-03-12 17:05:04 +1100

    I think that means the cli part works

---
+++ packages/create-twilio-function/src/cli.js
@@ -0,0 +1,38 @@
+const yargs = require('yargs');
+const createTwilioFunction = require('./create-twilio-function');
+
+function cli() {
+  yargs.help();
+  yargs.alias('h', 'help');
+  yargs.version();
+  yargs.alias('v', 'version');
+
+  yargs.usage('Creates a new Twilio Function project');
+  yargs.command(
+    '$0 <name>',
+    'Creates a new Twilio Function project',
+    command => {
+      command.positional('name', {
+        describe: 'Name of your project.',
+        type: 'string'
+      });
+      command.options({
+        accountSid: {
+          alias: 'a',
+          describe: 'The Account SID for your Twilio account',
+          type: 'string'
+        },
+        authToken: {
+          alias: 't',
+          describe: 'Your Twilio account Auth Token',
+          type: 'string'
+        }
+      });
+    },
+    argv => createTwilioFunction(argv)
+  );
+
+  return yargs;
+}
+
+module.exports = cli;
