c8169aa88aa06a1981dfb8bdc89af1395ed78039
dkundel <dominik.kundel@gmail.com>
2019-04-08 17:57:32 -0700

    :sparkles: Add node version check

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,26 @@
 const yargs = require('yargs');
+const chalk = require('chalk');
 
 const startCommand = require('./commands/start');
 const newCommand = require('./commands/new');
+const deployCommand = require('./commands/deploy');
 
 async function run(rawArgs) {
+  const nodeVersion = process.versions.node;
+  if (!nodeVersion.startsWith('8.10')) {
+    const msg = chalk`
+{red.bold INVALID NODE.JS Version}
+
+You are currently running ${nodeVersion} but the Twilio Runtime is 8.10.
+
+Please use a tool like nvm (https://github.com/creationix/nvm) to switch your version of Node.js.
+`.trim();
+    console.error(msg);
+    process.exit(1);
+  }
   yargs
     .command(newCommand)
+    .command(deployCommand)
     .command(startCommand)
     .parse(rawArgs.slice(2));
 }
