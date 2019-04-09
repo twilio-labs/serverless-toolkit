8520c8cb69b9c272dad996adb52af916b68e6457
dkundel <dominik.kundel@gmail.com>
2019-04-09 16:56:04 -0700

    Add production warning

---
+++ packages/serverless-api/scripts/not-for-production-warning.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+console.log(`
+------------------------------------------
+
+⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️   \u001b[33mWarning\u001b[39m   ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️ ⚠️
+
+This is a module for an experimental API
+that is still in preview mode. 
+
+You won't be able to use this library
+unless you have been granted prior access
+to the underlying API.
+
+------------------------------------------
+`);
