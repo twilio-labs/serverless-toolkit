ce2e17151520f5722327692f5f3c55f7dbbceef0
dkundel <dominik.kundel@gmail.com>
2019-07-07 21:05:24 -0700

    feat(types): add Context type

--- packages/serverless-runtime-types/types.d.ts
+++ packages/serverless-runtime-types/types.d.ts
@@ -1,3 +1,4 @@
+import twilio from 'twilio';
 import { ServiceContext } from 'twilio/lib/rest/sync/v1/service';
 
 export type ResourceMap = {
@@ -10,4 +11,10 @@ export type RuntimeInstance = {
   getAssets(): ResourceMap;
   getFunctions(): ResourceMap;
   getSync(config?: { serviceName: string }): ServiceContext;
+};
+
+export type Context = {
+  getTwilioClient(): twilio.Twilio;
+  DOMAIN_NAME: string;
+  [key: string]: string;
 };
