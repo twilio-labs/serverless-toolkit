853956bfff444842f1b08882a5a286cfad1d3c2c
dkundel <dominik.kundel@gmail.com>
2019-07-10 18:40:39 -0700

    fix: move Node.js version warning to consistent look

--- src/checks/nodejs-version.ts
+++ src/checks/nodejs-version.ts
@@ -1,8 +1,5 @@
-import boxen from 'boxen';
-import chalk from 'chalk';
 import { stripIndent } from 'common-tags';
-import size from 'window-size';
-import wrapAnsi from 'wrap-ansi';
+import { warningMessage } from '../printers/utils';
 
 const SERVERLESS_NODE_JS_VERSION = '8.10';
 
@@ -10,25 +7,17 @@ export function printVersionWarning(
   nodeVersion: string,
   expectedVersion: string
 ): void {
-  const msg = chalk`
-      {underline.bold {yellow WARNING!} {bold Different Node.js version}}
-
+  const title = 'Different Node.js Version Found';
+  const msg = stripIndent`
       You are currently running Node.js ${nodeVersion} on this local machine. The production environment for Twilio Serverless is currently on ${expectedVersion}.
 
       When you deploy to Twilio Serverless, you may encounter differences between local development and production.
 
       For a more accurate local development experience, please switch your Node.js version.
       A tool like nvm (https://github.com/creationix/nvm) can help.
-      `;
-  const wrappedMsg = wrapAnsi(msg, size.width - 20);
-  console.error(
-    boxen(stripIndent(wrappedMsg), {
-      padding: 1,
-      margin: 1,
-      borderStyle: 'double',
-      float: 'center',
-    })
-  );
+  `;
+
+  console.warn(warningMessage(title, msg));
 }
 
 export default function checkNodejsVersion() {
