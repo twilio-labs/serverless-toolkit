56df438ef2ca94f81185a95c124c59b0c0673548
dkundel <dominik.kundel@gmail.com>
2019-07-23 20:38:17 -0700

    fix(types): expose friendly_name on environments

--- packages/serverless-api/src/types/serverless-api.ts
+++ packages/serverless-api/src/types/serverless-api.ts
@@ -37,6 +37,7 @@ export interface ServiceList {
 export interface EnvironmentResource extends ResourceBase {
   unique_name: string;
   domain_name: string;
+  friendly_name: string;
   build_sid: string;
   domain_suffix: string;
 }
