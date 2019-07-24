998f83a6c52fafd08af2484f4c34bd4b7543b5dc
Dominik Kundel <dominik.kundel@gmail.com>
2019-07-24 12:28:55 -0700

    fix(api): use new uploads API (#9)
    * fix(api): use new uploads API
    
    This change is using the new serverless-upload API instead of the AWS S3 upload. This drops also the
    support for the AWS Upload function
    
    BREAKING CHANGE: Removes uploadToAws as a function

--- packages/serverless-api/src/api/assets.ts
+++ packages/serverless-api/src/api/assets.ts
@@ -3,7 +3,7 @@
 const { promisfy } = require('util');
 
 import debug from 'debug';
-import { extname } from 'path';
+import FormData from 'form-data';
 import {
   AssetApiResource,
   AssetList,
@@ -13,7 +13,7 @@ import {
   Sid,
   VersionResource,
 } from '../types';
-import { uploadToAws } from '../utils/aws-upload';
+import { getContentType } from '../utils/content-type';
 
 const log = debug('twilio-serverless-api:assets');
 
@@ -126,19 +126,38 @@ async function createAssetVersion(
   serviceSid: string,
   client: GotClient
 ): Promise<VersionResource> {
+  if (asset.access === 'protected') {
+    throw new Error(`Asset ${asset.name} cannot be "protected".
+Please change it to have "private" or "public" access.`);
+  }
   try {
+    const contentType = getContentType(
+      asset.content,
+      asset.filePath || asset.name
+    );
+    log('Uploading asset via form data with content-type "%s"', contentType);
+
+    const contentOpts = {
+      filename: asset.name,
+      contentType: contentType,
+    };
+
+    const form = new FormData();
+    form.append('path', asset.path);
+    form.append('visibility', asset.access);
+    form.append('content', asset.content, contentOpts);
+
     const resp = await client.post(
       `/Services/${serviceSid}/Assets/${asset.sid}/Versions`,
       {
-        form: true,
-        body: {
-          Path: asset.path,
-          Visibility: asset.access,
-        },
+        baseUrl: 'https://serverless-upload.twilio.com/v1',
+        body: form,
+        //@ts-ignore
+        json: false,
       }
     );
 
-    return (resp.body as unknown) as VersionResource;
+    return JSON.parse(resp.body) as VersionResource;
   } catch (err) {
     log('%O', err);
     throw new Error('Failed to upload Asset');
@@ -160,12 +179,5 @@ export async function uploadAsset(
   client: GotClient
 ): Promise<Sid> {
   const version = await createAssetVersion(asset, serviceSid, client);
-  const { pre_signed_upload_url: awsData } = version;
-  const awsResult = await uploadToAws(
-    awsData.url,
-    awsData.kmsARN,
-    asset.content,
-    asset.name
-  );
   return version.sid;
 }
