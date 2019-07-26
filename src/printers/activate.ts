709acd39608303f7ac59198d9ea5d3b97ccb0ece
dkundel <dominik.kundel@gmail.com>
2019-07-25 22:29:23 -0700

    fix(activate): redact printed auth token

--- src/printers/activate.ts
+++ src/printers/activate.ts
@@ -1,11 +1,12 @@
 import { ActivateConfig } from '@twilio-labs/serverless-api';
 import { stripIndent } from 'common-tags';
+import { redactPartOfString } from './utils';
 import chalk = require('chalk');
 
 export function printActivateConfig(config: ActivateConfig) {
   const message = chalk`
     {cyan.bold Account} ${config.accountSid}
-    {cyan.bold Token}   ${config.authToken}
+    {cyan.bold Token}   ${redactPartOfString(config.authToken)}
   `;
   process.stderr.write(stripIndent(message) + '\n');
 }
