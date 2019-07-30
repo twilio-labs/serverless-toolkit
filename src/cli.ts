aecb1bb54781c42dfe5dfedccfa2cc80a4a126cb
Phil Nash <philnash@gmail.com>
2019-07-31 05:08:10 +1000

    refactor: Splits list-templates command from new --list. (#55)
    This will let us share the list-templates command with other projects and the CLI plugin more easily.

--- src/cli.ts
+++ src/cli.ts
@@ -4,14 +4,16 @@ import * as DeployCommand from './commands/deploy';
 import * as ListCommand from './commands/list';
 import * as NewCommand from './commands/new';
 import * as StartCommand from './commands/start';
+import * as ListTemplatesCommand from './commands/list-templates';
 import './utils/debug';
 
 export async function run(rawArgs: string[]) {
   yargs
+    .command(StartCommand)
     .command(NewCommand)
+    .command(ListTemplatesCommand)
     .command(DeployCommand)
     .command(ListCommand)
     .command(ActivateCommand)
-    .command(StartCommand)
     .parse(rawArgs.slice(2));
 }
