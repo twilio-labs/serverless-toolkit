c17d3c63d89a00deb21c3cb39fb4da032eb7a7eb
dkundel <dominik.kundel@gmail.com>
2019-07-31 11:41:22 -0700

    chore: merge commit

---
+++ packages/plugin-serverless/src/commands/serverless/list-templates.js
@@ -0,0 +1,37 @@
+const { Command } = require('@oclif/command');
+
+const {
+  handler,
+  cliInfo,
+  describe,
+} = require('twilio-run/dist/commands/list-templates');
+const {
+  convertYargsOptionsToOclifFlags,
+  normalizeFlags,
+} = require('../../utils');
+
+class FunctionsListTemplates extends Command {
+  constructor(argv, config, secureStorage) {
+    super(argv, config, secureStorage);
+
+    this.showHeaders = true;
+  }
+
+  async run() {
+    let { flags, args } = this.parse(FunctionsListTemplates);
+    flags = normalizeFlags(flags);
+
+    const opts = Object.assign({}, flags, args);
+    return handler(opts, undefined);
+  }
+}
+
+FunctionsListTemplates.description = describe;
+
+FunctionsListTemplates.args = [];
+
+FunctionsListTemplates.flags = Object.assign(
+  convertYargsOptionsToOclifFlags(cliInfo.options)
+);
+
+module.exports = FunctionsListTemplates;
