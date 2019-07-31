95020c234d26e93068b745d4950af46661907301
Phil Nash <philnash@gmail.com>
2019-07-31 11:09:50 +1000

    refactor: renames "bundle" to "namespace" when downloading templates

--- src/templating/actions.ts
+++ src/templating/actions.ts
@@ -4,13 +4,15 @@ import chalk from 'chalk';
 
 export async function downloadTemplate(
   templateName: string,
-  bundleName: string,
+  namespace: string,
   targetDirectory: string
 ): Promise<void> {
   const files = await getTemplateFiles(templateName);
   try {
-    await writeFiles(files, targetDirectory, bundleName);
-    console.log(chalk`{green SUCCESS} Created new bundle ${bundleName}`);
+    await writeFiles(files, targetDirectory, namespace);
+    console.log(
+      chalk`{green SUCCESS} Downloaded new template into the "${namespace}" subdirectories`
+    );
   } catch (err) {
     console.error(chalk`{red ERROR} ${err.message}`);
   }
