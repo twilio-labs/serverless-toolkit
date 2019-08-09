637327f6329f15bee496466214975d74d6f63fdf
Phil Nash <philnash@gmail.com>
2019-08-09 15:14:14 -0700

    Reorganises and simplifies importCredentials

--- packages/create-twilio-function/src/create-twilio-function/import-credentials.js
+++ packages/create-twilio-function/src/create-twilio-function/import-credentials.js
@@ -1,35 +1,35 @@
 const inquirer = require('inquirer');
 
-async function importCredentials(config) {
-  if (
-    config.skipCredentials ||
-    (typeof process.env.TWILIO_ACCOUNT_SID === 'undefined' &&
-      typeof process.env.TWILIO_AUTH_TOKEN === 'undefined')
-  ) {
-    return {};
+const questions = [
+  {
+    type: 'confirm',
+    name: 'importCredentials',
+    message:
+      'Your account credentials have been found in your environment variables. Import them?',
+    default: true
   }
+];
+
+async function importCredentials(config) {
+  if (config.skipCredentials) return {};
 
   const credentials = {
     accountSid: process.env.TWILIO_ACCOUNT_SID,
     authToken: process.env.TWILIO_AUTH_TOKEN
   };
+  if (
+    typeof credentials.accountSid === 'undefined' &&
+    typeof credentials.authToken === 'undefined'
+  ) {
+    return {};
+  }
+
   if (config.importCredentials) {
     return credentials;
   }
-  const { importCredentials } = await inquirer.prompt([
-    {
-      type: 'confirm',
-      name: 'importCredentials',
-      message:
-        'Your account credentials have been found in your environment variables. Import them?',
-      default: true
-    }
-  ]);
-  if (importCredentials) {
-    return credentials;
-  } else {
-    return {};
-  }
+
+  const { importCredentials } = await inquirer.prompt(questions);
+  return importCredentials ? credentials : {};
 }
 
 module.exports = importCredentials;
