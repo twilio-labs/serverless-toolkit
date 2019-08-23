71de3d8df7d338b522ae212368406f82ba39bbdf
Phil Nash <philnash@gmail.com>
2019-08-24 07:39:06 +1000

    Allow only project names that have letters, numbers and hyphens (#23)
    * Only allow project names that have letters, numbers and hyphens
    
    * Fully validates project name

---
+++ packages/create-twilio-function/src/create-twilio-function/validate-project-name.js
@@ -0,0 +1,40 @@
+function validateProjectName(name) {
+  let valid = true;
+  const errors = [];
+  if (!assertNotLongerThan(name, 32)) {
+    valid = false;
+    errors.push('must be shorter than 32 characters');
+  }
+  if (!assertContainsLettersNumbersHyphens(name)) {
+    valid = false;
+    errors.push('must only include letters, numbers and hyphens');
+  }
+  if (!assertDoesntStartWithHyphen(name)) {
+    valid = false;
+    errors.push('must not start with a hyphen');
+  }
+  if (!assertDoesntEndWithHyphen(name)) {
+    valid = false;
+    errors.push('must not end with a hyphen');
+  }
+  return { valid, errors };
+}
+
+function assertContainsLettersNumbersHyphens(name) {
+  const nameRegex = /^[A-Za-z0-9\-]+$/;
+  return !!name.match(nameRegex);
+}
+
+function assertDoesntStartWithHyphen(name) {
+  return !name.startsWith('-');
+}
+
+function assertDoesntEndWithHyphen(name) {
+  return !name.endsWith('-');
+}
+
+function assertNotLongerThan(name, chars = 32) {
+  return name.length <= chars;
+}
+
+module.exports = validateProjectName;
--- packages/create-twilio-function/tests/create-twilio-function.test.js
+++ packages/create-twilio-function/tests/create-twilio-function.test.js
@@ -52,205 +52,266 @@ afterEach(async () => {
 });
 
 describe('createTwilioFunction', () => {
-  beforeEach(() => {
-    inquirer.prompt = jest.fn(() =>
-      Promise.resolve({
-        accountSid: 'test-sid',
-        authToken: 'test-auth-token'
-      })
-    );
+  describe('with an acceptable project name', () => {
+    beforeEach(() => {
+      inquirer.prompt = jest.fn(() =>
+        Promise.resolve({
+          accountSid: 'test-sid',
+          authToken: 'test-auth-token'
+        })
+      );
 
-    nock('https://raw.githubusercontent.com')
-      .get('/github/gitignore/master/Node.gitignore')
-      .reply(200, '*.log\n.env');
-  });
+      nock('https://raw.githubusercontent.com')
+        .get('/github/gitignore/master/Node.gitignore')
+        .reply(200, '*.log\n.env');
+    });
 
-  it('scaffolds a Twilio Function', async () => {
-    const name = 'test-function';
-    await createTwilioFunction({ name, path: './scratch' });
+    it('scaffolds a Twilio Function', async () => {
+      const name = 'test-function';
+      await createTwilioFunction({ name, path: './scratch' });
 
-    const dir = await stat(`./scratch/${name}`);
-    expect(dir.isDirectory());
-    const env = await stat(`./scratch/${name}/.env`);
-    expect(env.isFile());
-    const nvmrc = await stat(`./scratch/${name}/.nvmrc`);
-    expect(nvmrc.isFile());
+      const dir = await stat(`./scratch/${name}`);
+      expect(dir.isDirectory());
+      const env = await stat(`./scratch/${name}/.env`);
+      expect(env.isFile());
+      const nvmrc = await stat(`./scratch/${name}/.nvmrc`);
+      expect(nvmrc.isFile());
 
-    const packageJSON = await stat(`./scratch/${name}/package.json`);
-    expect(packageJSON.isFile());
+      const packageJSON = await stat(`./scratch/${name}/package.json`);
+      expect(packageJSON.isFile());
 
-    const gitignore = await stat(`./scratch/${name}/.gitignore`);
-    expect(gitignore.isFile());
+      const gitignore = await stat(`./scratch/${name}/.gitignore`);
+      expect(gitignore.isFile());
 
-    const functions = await stat(`./scratch/${name}/functions`);
-    expect(functions.isDirectory());
+      const functions = await stat(`./scratch/${name}/functions`);
+      expect(functions.isDirectory());
 
-    const assets = await stat(`./scratch/${name}/assets`);
-    expect(assets.isDirectory());
+      const assets = await stat(`./scratch/${name}/assets`);
+      expect(assets.isDirectory());
 
-    const example = await stat(`./scratch/${name}/functions/hello-world.js`);
-    expect(example.isFile());
+      const example = await stat(`./scratch/${name}/functions/hello-world.js`);
+      expect(example.isFile());
 
-    const asset = await stat(`./scratch/${name}/assets/index.html`);
-    expect(asset.isFile());
+      const asset = await stat(`./scratch/${name}/assets/index.html`);
+      expect(asset.isFile());
 
-    expect(installDependencies).toHaveBeenCalledWith(`scratch/${name}`);
+      expect(installDependencies).toHaveBeenCalledWith(`scratch/${name}`);
 
-    expect(console.log).toHaveBeenCalledWith('success message');
-  });
+      expect(console.log).toHaveBeenCalledWith('success message');
+    });
 
-  it('scaffolds a Twilio Function with a template', async () => {
-    const gitHubAPI = nock('https://api.github.com');
-    gitHubAPI
-      .get('/repos/twilio-labs/function-templates/contents/blank')
-      .reply(200, [
-        {
-          name: 'functions'
-        },
-        {
-          name: '.env',
-          download_url:
-            'https://raw.githubusercontent.com/twilio-labs/function-templates/master/blank/.env'
-        }
-      ]);
-    gitHubAPI
-      .get('/repos/twilio-labs/function-templates/contents/blank/functions')
-      .reply(200, [
-        {
-          name: 'blank.js',
-          download_url:
-            'https://raw.githubusercontent.com/twilio-labs/function-templates/master/blank/functions/blank.js'
-        }
-      ]);
-    const gitHubRaw = nock('https://raw.githubusercontent.com');
-    gitHubRaw
-      .get('/twilio-labs/function-templates/master/blank/functions/blank.js')
-      .reply(
-        200,
-        `exports.handler = function(context, event, callback) {
+    it('scaffolds a Twilio Function with a template', async () => {
+      const gitHubAPI = nock('https://api.github.com');
+      gitHubAPI
+        .get('/repos/twilio-labs/function-templates/contents/blank')
+        .reply(200, [
+          {
+            name: 'functions'
+          },
+          {
+            name: '.env',
+            download_url:
+              'https://raw.githubusercontent.com/twilio-labs/function-templates/master/blank/.env'
+          }
+        ]);
+      gitHubAPI
+        .get('/repos/twilio-labs/function-templates/contents/blank/functions')
+        .reply(200, [
+          {
+            name: 'blank.js',
+            download_url:
+              'https://raw.githubusercontent.com/twilio-labs/function-templates/master/blank/functions/blank.js'
+          }
+        ]);
+      const gitHubRaw = nock('https://raw.githubusercontent.com');
+      gitHubRaw
+        .get('/twilio-labs/function-templates/master/blank/functions/blank.js')
+        .reply(
+          200,
+          `exports.handler = function(context, event, callback) {
   callback(null, {});
 };`
+        );
+      gitHubRaw
+        .get('/github/gitignore/master/Node.gitignore')
+        .reply(200, 'node_modules/');
+      gitHubRaw
+        .get('/twilio-labs/function-templates/master/blank/.env')
+        .reply(200, '');
+
+      const name = 'test-function';
+      await createTwilioFunction({
+        name,
+        path: './scratch',
+        template: 'blank'
+      });
+
+      const dir = await stat(`./scratch/${name}`);
+      expect(dir.isDirectory());
+      const env = await stat(`./scratch/${name}/.env`);
+      expect(env.isFile());
+      const nvmrc = await stat(`./scratch/${name}/.nvmrc`);
+      expect(nvmrc.isFile());
+
+      const packageJSON = await stat(`./scratch/${name}/package.json`);
+      expect(packageJSON.isFile());
+
+      const gitignore = await stat(`./scratch/${name}/.gitignore`);
+      expect(gitignore.isFile());
+
+      const functions = await stat(`./scratch/${name}/functions`);
+      expect(functions.isDirectory());
+
+      const assets = await stat(`./scratch/${name}/assets`);
+      expect(assets.isDirectory());
+
+      const exampleFiles = await readdir(`./scratch/${name}/functions`);
+      expect(exampleFiles).toEqual(
+        expect.not.arrayContaining(['hello-world.js'])
       );
-    gitHubRaw
-      .get('/github/gitignore/master/Node.gitignore')
-      .reply(200, 'node_modules/');
-    gitHubRaw
-      .get('/twilio-labs/function-templates/master/blank/.env')
-      .reply(200, '');
 
-    const name = 'test-function';
-    await createTwilioFunction({
-      name,
-      path: './scratch',
-      template: 'blank'
+      const templateFunction = await stat(
+        `./scratch/${name}/functions/blank.js`
+      );
+      expect(templateFunction.isFile());
+
+      const exampleAssets = await readdir(`./scratch/${name}/assets`);
+      expect(exampleAssets).toEqual(expect.not.arrayContaining(['index.html']));
+
+      expect(installDependencies).toHaveBeenCalledWith(`scratch/${name}`);
+
+      expect(console.log).toHaveBeenCalledWith('success message');
     });
 
-    const dir = await stat(`./scratch/${name}`);
-    expect(dir.isDirectory());
-    const env = await stat(`./scratch/${name}/.env`);
-    expect(env.isFile());
-    const nvmrc = await stat(`./scratch/${name}/.nvmrc`);
-    expect(nvmrc.isFile());
+    it('handles a missing template gracefully', async () => {
+      const templateName = 'missing';
+      const name = 'test-function';
+      const gitHubAPI = nock('https://api.github.com');
+      gitHubAPI
+        .get(`/repos/twilio-labs/function-templates/contents/${templateName}`)
+        .reply(404);
 
-    const packageJSON = await stat(`./scratch/${name}/package.json`);
-    expect(packageJSON.isFile());
+      const fail = jest.spyOn(spinner, 'fail');
 
-    const gitignore = await stat(`./scratch/${name}/.gitignore`);
-    expect(gitignore.isFile());
+      await createTwilioFunction({
+        name,
+        path: './scratch',
+        template: templateName
+      });
 
-    const functions = await stat(`./scratch/${name}/functions`);
-    expect(functions.isDirectory());
+      expect.assertions(3);
 
-    const assets = await stat(`./scratch/${name}/assets`);
-    expect(assets.isDirectory());
-
-    const exampleFiles = await readdir(`./scratch/${name}/functions`);
-    expect(exampleFiles).toEqual(
-      expect.not.arrayContaining(['hello-world.js'])
-    );
-
-    const templateFunction = await stat(`./scratch/${name}/functions/blank.js`);
-    expect(templateFunction.isFile());
-
-    const exampleAssets = await readdir(`./scratch/${name}/assets`);
-    expect(exampleAssets).toEqual(expect.not.arrayContaining(['index.html']));
-
-    expect(installDependencies).toHaveBeenCalledWith(`scratch/${name}`);
-
-    expect(console.log).toHaveBeenCalledWith('success message');
-  });
-
-  it('handles a missing template gracefully', async () => {
-    const templateName = 'missing';
-    const name = 'test-function';
-    const gitHubAPI = nock('https://api.github.com');
-    gitHubAPI
-      .get(`/repos/twilio-labs/function-templates/contents/${templateName}`)
-      .reply(404);
-
-    const fail = jest.spyOn(spinner, 'fail');
-
-    await createTwilioFunction({
-      name,
-      path: './scratch',
-      template: templateName
+      expect(fail).toHaveBeenCalledTimes(1);
+      expect(fail).toHaveBeenCalledWith(
+        `The template "${templateName}" doesn't exist`
+      );
+      try {
+        await stat(`./scratch/${name}`);
+      } catch (e) {
+        expect(e.toString()).toMatch('no such file or directory');
+      }
     });
 
-    expect.assertions(3);
+    it("doesn't scaffold if the target folder name already exists", async () => {
+      const name = 'test-function';
+      await mkdir('./scratch/test-function');
+      const fail = jest.spyOn(spinner, 'fail');
 
-    expect(fail).toHaveBeenCalledTimes(1);
-    expect(fail).toHaveBeenCalledWith(
-      `The template "${templateName}" doesn't exist`
-    );
-    try {
-      await stat(`./scratch/${name}`);
-    } catch (e) {
-      expect(e.toString()).toMatch('no such file or directory');
-    }
+      await createTwilioFunction({ name, path: './scratch' });
+
+      expect.assertions(4);
+
+      expect(fail).toHaveBeenCalledTimes(1);
+      expect(fail).toHaveBeenCalledWith(
+        `A directory called '${name}' already exists. Please create your function in a new directory.`
+      );
+      expect(console.log).not.toHaveBeenCalled();
+
+      try {
+        await stat(`./scratch/${name}/package.json`);
+      } catch (e) {
+        expect(e.toString()).toMatch('no such file or directory');
+      }
+    });
+
+    it("fails gracefully if it doesn't have permission to create directories", async () => {
+      const name = 'test-function';
+      const chmod = promisify(fs.chmod);
+      await chmod('./scratch', 0o555);
+      const fail = jest.spyOn(spinner, 'fail');
+
+      await createTwilioFunction({ name, path: './scratch' });
+
+      expect.assertions(4);
+
+      expect(fail).toHaveBeenCalledTimes(1);
+      expect(fail).toHaveBeenCalledWith(
+        `You do not have permission to create files or directories in the path './scratch'.`
+      );
+      expect(console.log).not.toHaveBeenCalled();
+
+      try {
+        await stat(`./scratch/${name}/package.json`);
+      } catch (e) {
+        expect(e.toString()).toMatch('no such file or directory');
+      }
+    });
   });
 
-  it("doesn't scaffold if the target folder name already exists", async () => {
-    const name = 'test-function';
-    await mkdir('./scratch/test-function');
-    const fail = jest.spyOn(spinner, 'fail');
+  describe('with an unacceptable project name', () => {
+    beforeEach(() => {
+      inquirer.prompt = jest.fn();
+      inquirer.prompt
+        .mockReturnValueOnce(
+          Promise.resolve({
+            name: 'test-function'
+          })
+        )
+        .mockReturnValueOnce(
+          Promise.resolve({
+            accountSid: 'test-sid',
+            authToken: 'test-auth-token'
+          })
+        );
 
-    await createTwilioFunction({ name, path: './scratch' });
+      nock('https://raw.githubusercontent.com')
+        .get('/github/gitignore/master/Node.gitignore')
+        .reply(200, '*.log\n.env');
+    });
 
-    expect.assertions(4);
+    it('scaffolds a Twilio Function and prompts for a new name', async () => {
+      const badName = 'GreatTest!!!';
+      const name = 'test-function';
+      await createTwilioFunction({ name: badName, path: './scratch' });
 
-    expect(fail).toHaveBeenCalledTimes(1);
-    expect(fail).toHaveBeenCalledWith(
-      `A directory called '${name}' already exists. Please create your function in a new directory.`
-    );
-    expect(console.log).not.toHaveBeenCalled();
+      const dir = await stat(`./scratch/${name}`);
+      expect(dir.isDirectory());
+      const env = await stat(`./scratch/${name}/.env`);
+      expect(env.isFile());
+      const nvmrc = await stat(`./scratch/${name}/.nvmrc`);
+      expect(nvmrc.isFile());
 
-    try {
-      await stat(`./scratch/${name}/package.json`);
-    } catch (e) {
-      expect(e.toString()).toMatch('no such file or directory');
-    }
-  });
+      const packageJSON = await stat(`./scratch/${name}/package.json`);
+      expect(packageJSON.isFile());
 
-  it("fails gracefully if it doesn't have permission to create directories", async () => {
-    const name = 'test-function';
-    const chmod = promisify(fs.chmod);
-    await chmod('./scratch', 0o555);
-    const fail = jest.spyOn(spinner, 'fail');
+      const gitignore = await stat(`./scratch/${name}/.gitignore`);
+      expect(gitignore.isFile());
 
-    await createTwilioFunction({ name, path: './scratch' });
+      const functions = await stat(`./scratch/${name}/functions`);
+      expect(functions.isDirectory());
 
-    expect.assertions(4);
+      const assets = await stat(`./scratch/${name}/assets`);
+      expect(assets.isDirectory());
 
-    expect(fail).toHaveBeenCalledTimes(1);
-    expect(fail).toHaveBeenCalledWith(
-      `You do not have permission to create files or directories in the path './scratch'.`
-    );
-    expect(console.log).not.toHaveBeenCalled();
+      const example = await stat(`./scratch/${name}/functions/hello-world.js`);
+      expect(example.isFile());
 
-    try {
-      await stat(`./scratch/${name}/package.json`);
-    } catch (e) {
-      expect(e.toString()).toMatch('no such file or directory');
-    }
+      const asset = await stat(`./scratch/${name}/assets/index.html`);
+      expect(asset.isFile());
+
+      expect(installDependencies).toHaveBeenCalledWith(`scratch/${name}`);
+
+      expect(console.log).toHaveBeenCalledWith('success message');
+    });
   });
 });
