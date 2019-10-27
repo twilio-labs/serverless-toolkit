bbc232aaef937aa367fffa2e882b8c8cf90f2f4c
Phil Nash <philnash@gmail.com>
2019-10-28 09:19:00 +1100

    Merge branch 'daphnesmit-patch-1'

--- packages/create-twilio-function/src/create-twilio-function/window-size.js
+++ packages/create-twilio-function/src/create-twilio-function/window-size.js
@@ -2,16 +2,15 @@ const windowSize = require('window-size');
 
 function getWindowSize() {
   const defaultSize = { width: 80, height: 300 };
-  let currentSize = windowSize.get();
+  const currentSize = windowSize.get();
 
   if (!currentSize) {
     return defaultSize;
   }
-  else {
-    if(!currentSize.width) currentSize.width = defaultSize.width;
-    if(!currentSize.height) currentSize.height = defaultSize.height;
-    return currentSize;
-  }
+  
+  if(!currentSize.width) currentSize.width = defaultSize.width;
+  if(!currentSize.height) currentSize.height = defaultSize.height;
+  return currentSize;
 }
 
 module.exports = getWindowSize;
