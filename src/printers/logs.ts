ec4af4abc08892e266a48940e51362e2cb139cbd
Phil Nash <philnash@gmail.com>
2020-03-19 07:50:46 +1100

    feat(logs): adds command to access new logging functions (#110)
    * feat(logs): adds command to access new logging functions
    
    * improvement(logs): receive objects from log stream
    
    I updated the serverless api to emit objects not strings, so this needed
    
    * chore(logs): updates serverless-api for log methods
    
    * WIP logging
    
    * feat(logs): add JSON output format
    
    * fix(logs): corrects the parameter to filter by function
    
    * fixup(logs): write to stdout instead of stderr
    
    * fixup(logs): no need to cast to boolean when the types already work.
    
    * Delete package-lock.json
    
    Co-authored-by: Dominik Kundel <dkundel@twilio.com>

---
+++ src/printers/logs.ts
@@ -0,0 +1,19 @@
+import { LogList, LogApiResource } from '@twilio-labs/serverless-api';
+import { LogsConfig } from '../config/logs';
+import { writeOutput } from '../utils/output';
+
+export function printLogs(
+  result: LogApiResource[],
+  config: LogsConfig,
+  outputFormat?: string
+) {
+  result.forEach(log => printLog(log, outputFormat));
+}
+
+export function printLog(log: LogApiResource, outputFormat?: string) {
+  if (outputFormat === 'json') {
+    writeOutput(JSON.stringify(log));
+  } else {
+    writeOutput(`[${log.level}][${log.date_created}]: ${log.message}`);
+  }
+}
