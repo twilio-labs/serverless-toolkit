3d0011e24f43c6773b4bf7ebb204a0c94fb317d6
Phil Nash <philnash@gmail.com>
2020-03-19 16:28:41 +1100

    Merge pull request #29 from philnash/logs
    feat(logs): connects up twilio-run logs command

---
+++ packages/plugin-serverless/src/commands/serverless/logs.js
@@ -0,0 +1,34 @@
+const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
+
+const { handler, cliInfo, describe } = require('twilio-run/dist/commands/logs');
+
+const {
+  convertYargsOptionsToOclifFlags,
+  normalizeFlags,
+  createExternalCliOptions,
+} = require('../../utils');
+
+class LogsList extends TwilioClientCommand {
+  async run() {
+    await super.run();
+
+    let { flags, args } = this.parse(LogsList);
+    flags = normalizeFlags(flags);
+
+    const externalOptions = createExternalCliOptions(flags, this.twilioClient);
+
+    const opts = Object.assign({}, flags, args);
+    return handler(opts, externalOptions);
+  }
+}
+
+LogsList.description = describe;
+
+LogsList.args = [];
+
+LogsList.flags = Object.assign(
+  convertYargsOptionsToOclifFlags(cliInfo.options),
+  { profile: TwilioClientCommand.flags.profile }
+);
+
+module.exports = LogsList;
