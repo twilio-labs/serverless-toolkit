1bd18796232c49b15697d265052149648bc38e85
Phil Nash <philnash@gmail.com>
2020-05-09 15:57:09 +1000

    Small refactor for questions prompt

--- packages/create-twilio-function/src/create-twilio-function/prompt.js
+++ packages/create-twilio-function/src/create-twilio-function/prompt.js
@@ -10,25 +10,29 @@ function validateAccountSid(input) {
   return 'An Account SID starts with "AC".';
 }
 
+const accountSidQuestion = {
+  type: 'input',
+  name: 'accountSid',
+  message: 'Twilio Account SID',
+  validate: validateAccountSid,
+};
+
+const authTokenQuestion = {
+  type: 'password',
+  name: 'authToken',
+  message: 'Twilio auth token',
+};
+
 function promptForAccountDetails(config) {
   if (config.skipCredentials) {
     return {};
   }
   const questions = [];
   if (typeof config.accountSid === 'undefined') {
-    questions.push({
-      type: 'input',
-      name: 'accountSid',
-      message: 'Twilio Account SID',
-      validate: validateAccountSid,
-    });
+    questions.push(accountSidQuestion);
   }
   if (typeof config.authToken === 'undefined') {
-    questions.push({
-      type: 'password',
-      name: 'authToken',
-      message: 'Twilio auth token',
-    });
+    questions.push(authTokenQuestion);
   }
   if (questions.length > 0) {
     console.log(
