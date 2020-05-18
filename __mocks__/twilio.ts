534df7627a6dd16425fdee418c384b8474acc70b
Dominik Kundel <dkundel@twilio.com>
2020-05-18 14:30:40 -0700

    test(mocks): fix route tests by fixing mock

--- __mocks__/twilio.ts
+++ __mocks__/twilio.ts
@@ -1,4 +1,7 @@
-const twilio = jest.genMockFromModule('twilio');
+const actualTwilio = jest.requireActual('twilio');
+const twilio: any = jest.genMockFromModule('twilio');
+
+twilio['twiml'] = actualTwilio.twiml;
 
 // mock specific functionality
 
