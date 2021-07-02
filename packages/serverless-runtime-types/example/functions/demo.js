c379aa5445fc71e13ce58a829cb11f4caadeb051
Dominik Kundel <dkundel@twilio.com>
2021-07-02 12:08:45 -0700

    feat(runtime-types): add cookie/header support for types

--- packages/serverless-runtime-types/example/functions/demo.js
+++ packages/serverless-runtime-types/example/functions/demo.js
@@ -2,11 +2,12 @@
 
 /**
  * @param {import('@twilio-labs/serverless-runtime-types').Context} context
- * @param {{}} event
+ * @param {import('@twilio-labs/serverless-runtime-types').ServerlessEventObject<{}, {}, { token: string }>} event
  * @param {import('@twilio-labs/serverless-runtime-types').ServerlessCallback} callback
  */
-exports.handler = function(context, event, callback) {
+exports.handler = function (context, event, callback) {
   let twiml = new Twilio.twiml.MessagingResponse();
+  console.log(event.cookies.token);
   twiml.message('Hello World');
   callback(null, twiml);
 };
