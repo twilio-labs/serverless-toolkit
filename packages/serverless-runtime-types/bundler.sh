9432e1200d3862b925aafe48eea69ab99967d787
mbeldartwilio <mbeldar@twilio.com>
2021-10-06 13:56:11 -0600

    feat(serverless-runtime-types): Cleaning and making executable shell script.

--- packages/serverless-runtime-types/bundler.sh
+++ packages/serverless-runtime-types/bundler.sh
@@ -3,9 +3,7 @@
 # bundle.d.ts can be added into Monaco/react editor, until Monaco supports
 # importing multiple files.
 
-# Remove the import statements from index.d.ts and save it to index.temp.d.ts
-awk -v RS= '!/^import /' ORS='\n\n' index.d.ts > index.temp.d.ts
-# Merge types.d.ts and index.temp.d.ts into bundle.d.ts
-(echo "$(cat types.d.ts)"; echo; echo "$(cat index.temp.d.ts)") > bundle.d.ts
-# Remvoe the temp file
-rm -f index.temp.d.ts
+# Append types.d.ts into bundle.d.ts
+(cat types.d.ts && echo) > bundle.d.ts
+# Remove the import statements from index.d.ts and append it to bundle.d.ts
+(awk -v RS= '!/^import /' ORS='\n' index.d.ts) >> bundle.d.ts
