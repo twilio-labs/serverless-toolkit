ed65326b7280013791f01be8cf64e7f09503fe97
mbeldartwilio <mbeldar@twilio.com>
2021-10-08 12:02:45 -0600

    feat: using node script instead of shell. (Windows support)

--- packages/serverless-runtime-types/bundler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Shell script to merge all the types files into bundle.d.ts
-# bundle.d.ts can be added into Monaco/react editor, until Monaco supports
-# importing multiple files.
-
-# Append types.d.ts into bundle.d.ts
-(cat types.d.ts && echo) > bundle.d.ts
-# Remove the import statements from index.d.ts and append it to bundle.d.ts
-(awk -v RS= '!/^import /' ORS='\n' index.d.ts) >> bundle.d.ts
