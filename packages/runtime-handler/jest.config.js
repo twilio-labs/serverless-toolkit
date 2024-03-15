d9609b5612e47e28cfcc120e01bd0955d89547f2
Victor Ayogu <vayogu@twilio.com>
2024-03-15 15:26:19 +0200

    fix: update snapshots and tests

--- packages/runtime-handler/jest.config.js
+++ packages/runtime-handler/jest.config.js
@@ -2,10 +2,13 @@ const base = require('../../jest.config.base.js');
 
 module.exports = {
   ...base,
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.test.json',
-    },
+  transform: {
+    '^.+\\.ts?$': [
+      'ts-jest',
+      {
+        tsConfig: './tsconfig.test.json',
+      },
+    ],
   },
   name: 'runtime-handler',
   displayName: 'runtime-handler',
