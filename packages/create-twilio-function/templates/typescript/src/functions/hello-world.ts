0879513bd2991caebe1e5d348964a6978374fb53
Andre Lars Da Cunha <adacunha@twilio.com>
2025-01-24 14:43:33 +0100

    fix(create-twilio-function): fix hello-world template

--- packages/create-twilio-function/templates/typescript/src/functions/hello-world.ts
+++ packages/create-twilio-function/templates/typescript/src/functions/hello-world.ts
@@ -5,25 +5,30 @@ import {
   Context,
   ServerlessCallback,
   ServerlessFunctionSignature,
+  ServerlessEventObject,
 } from '@twilio-labs/serverless-runtime-types/types';
 
 type MyEvent = {
-  Body?: string
-}
+  Body?: string;
+};
 
 // If you want to use environment variables, you will need to type them like
 // this and add them to the Context in the function signature as
 // Context<MyContext> as you see below.
 type MyContext = {
-  GREETING?: string
-}
+  GREETING?: string;
+};
 
-export const handler: ServerlessFunctionSignature = function(
+export const handler: ServerlessFunctionSignature = function (
   context: Context<MyContext>,
-  event: MyEvent,
+  event: ServerlessEventObject<MyEvent>,
   callback: ServerlessCallback
 ) {
   const twiml = new Twilio.twiml.VoiceResponse();
-  twiml.say(`${context.GREETING ? context.GREETING : 'Hello'} ${event.Body ? event.Body : 'World'}!`);
+  twiml.say(
+    `${context.GREETING ? context.GREETING : 'Hello'} ${
+      event.Body ? event.Body : 'World'
+    }!`
+  );
   callback(null, twiml);
 };
