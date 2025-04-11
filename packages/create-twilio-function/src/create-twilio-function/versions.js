d1c5369f5c3682a5aaa0ad040efe9d94597024e5
Janno Teelem <jteelem@twilio.com>
2025-04-11 19:35:10 +0300

    Update toolkit to default to node22

--- packages/create-twilio-function/src/create-twilio-function/versions.js
+++ packages/create-twilio-function/src/create-twilio-function/versions.js
@@ -6,7 +6,7 @@ module.exports = {
     '@twilio/runtime-handler'
   ].replace(/[\^~]/, ''),
   twilioRun: pkgJson.dependencies['twilio-run'],
-  node: '18',
+  node: '22',
   typescript: '^5.3.3',
   serverlessRuntimeTypes: '^4.0.0',
   copyfiles: '^2.4.1',
